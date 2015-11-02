b84b3ac057ec73786253b643ab2ace3658dfa2e2
Michał Sakwa <msakwa@insolutions.pl>
2015-11-02 09:35:00 +0100

    Add project to source control.

---
+++ SampleProject/Controllers/SiteController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleProject.Controllers
+{
+    public class SiteController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(string id, int age)
+        {
+            return View(new InputData() { Name = id, Age = age });
+        }
+
+        [HttpPost]
+        public ActionResult Edit(InputData vrpInput)
+        {
+            vrpInput.Name += " po edycji";
+            vrpInput.Age++;
+
+            return RedirectToAction("Edit", "Site", new { id = vrpInput.Name, age = vrpInput.Age });
+        }
+    }
+
+    public class InputData
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
