633bed38a8c22c27006af2a9a33b6546a26fe138
Arkadiusz Neuman <aneuman@insolutions.pl>
2015-11-16 10:19:38 +0100

    Added users table.

---
+++ SampleProject/Controllers/PersonController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SampleProject.Utilities;
+
+namespace SampleProject.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View(new NamesGenerator().GetRandomPersons());
+        }
+    }
+}
