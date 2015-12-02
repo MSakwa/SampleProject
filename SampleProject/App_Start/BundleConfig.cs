c18357a77eeefc7329d9ff535563b6bf3c42bebd
Arkadiusz Neuman <aneuman@insolutions.pl>
2015-12-02 09:02:44 +0100

    Added simple bootstrap lesson.

---
+++ SampleProject/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SampleProject.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection vrpBundleCollection)
+        {
+            vrpBundleCollection.Add(new StyleBundle("~/MojeCss")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/bootstrap-theme.css"));
+        }
+    }
+}
--- SampleProject/Global.asax.cs
+++ SampleProject/Global.asax.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using SampleProject.App_Start;
 
 namespace SampleProject
 {
@@ -13,6 +15,7 @@ namespace SampleProject
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
